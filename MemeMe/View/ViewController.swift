0b92e2e3d96ee791bd7903525f0d238538f6e733
andremartingo <andre.martingo@gmail.com>
2018-04-05 20:49:38 +0100

    implement textFields color and stroke

--- MemeMe/View/ViewController.swift
+++ MemeMe/View/ViewController.swift
@@ -9,28 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate{
 
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var imagePickerView: UIImageView!
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let memeTextAttributes:[String: Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: 5]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         super.viewDidLoad()
-        imagePickerView.contentMode = .scaleAspectFit
+        imagePickerView.contentMode = .scaleAspectFill
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         textFieldInit(topTextField, initalText: "TOP")
         textFieldInit(bottomTextField, initalText: "BOTTOM")
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // MARK: Textfield Config
     func textFieldInit(_ textField: UITextField, initalText: String){
         textField.text = initalText
         textField.textAlignment = .center
+        textField.delegate = self
+        textField.defaultTextAttributes = self.memeTextAttributes
+        textField.autocapitalizationType = .allCharacters
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: ImagePicker Config
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -45,6 +65,7 @@ UINavigationControllerDelegate {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: Called when image is selected
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]){
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -55,15 +76,6 @@ UINavigationControllerDelegate {
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
         dismiss(animated: true, completion: nil)
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField){
-        textField.text = ""
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        textField.resignFirstResponder()
-        return true
     }
 
 }
