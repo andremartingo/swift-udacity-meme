b737a3a8c591834b2d940ab9fd7e7b84a3aa6e92
andremartingo <andre.martingo@gmail.com>
2018-05-31 20:10:28 +0100

    bitrise test

---
+++ MemeMe/View/MemeCollectionView.swift
@@ -0,0 +1,57 @@
+//
+//  MemeCollectionView.swift
+//  MemeMe
+//
+//  Created by André Martingo on 08/05/2018.
+//  Copyright © 2018 André Martingo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+    // MARK: Properties
+    var memes = [Meme]()
+    var getAllMemesController : GetAllMemesController!
+
+    // MARK: Table View Data Source
+    override func viewWillAppear(_ animated: Bool) {
+        memes = getAllMemesController.getAllMemes()
+        collectionView?.reloadData()
+    }
+    override func viewDidLoad() {
+        getAllMemesController = GetAllMemesController()
+        navigationItem.title = "Memes"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add , target: self, action: #selector(addTapped))
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.memeImageView.image = meme.memedImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        let detailViewController = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        detailViewController.meme = self.memes[indexPath.row]
+        navigationController!.pushViewController(detailViewController, animated: true)
+        
+    }
+    
+    @objc func addTapped() {
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        navigationController!.pushViewController(viewController, animated: true)        }
+}
